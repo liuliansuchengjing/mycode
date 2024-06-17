0752e5a9c321f4dc2b5c9f490aacb9e178617698
liuliansuchengjing <159008886+liuliansuchengjing@users.noreply.github.com>
2024-06-17 13:31:31 +0800

    Update HGAT.py

--- HGAT.py
+++ HGAT.py
@@ -50,7 +50,8 @@ class DJconv(nn.Module):
         Du_e = Du_e.to(torch.float32)
 
         U = U.to(torch.float32)
-        M_u = torch.linalg.multi_dot([Du_v, Hu, Du_e, Du_e, Hu.t(), Du_v, U]) + U
+        # M_u = torch.linalg.multi_dot([Du_v.cuda(), U]) + U
+        M_u = torch.linalg.multi_dot([Du_v.cuda(), Hu.cuda(), Du_e.cuda(), Du_e.cuda(), Hu.t().cuda(), Du_v.cuda(), U]) + U
 
         U_out = torch.matmul(M_u.cuda(), self.weight.cuda()) + self.bias.cuda()
 
